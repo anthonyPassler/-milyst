6cf8e78b17b9834fdc5926a8f4d395d13bae573b
Anthony Passler <passleranthony@yahoo.com>
2020-07-27 17:05:38 +0200

    testing reset emailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,16 @@
-class Notifier < Devise::Mailer
+class Notifier < ActionMailer::Base
+  include Devise::Mailers::Helpers
 
   # send password reset instructions
-  def reset_password_instructions(user)
-     @resource = user
-     mail(:to => @resource.email, :subject => "Reset password instructions", :tag => 'password-reset', :content_type => "text/html") do |format|
-       format.html { render "devise/mailer/reset_password_instructions" }
-     end
-   end
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+  def reset_password_instructions(record,token,options)
+    devise_mail(record, :reset_password_instructions)
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
+  end
 end
